71c43d7e9b7b22c0e15f3d28d448a622bd0bbf90
Junyoung <codesep8@gmail.com>
2025-04-18 01:41:05 +0000

    dd

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>root</h1>
+        <Layout />
+    </div>
+</template>
+
+<script>
+import Liberty from "../skins/liberty/layout.vue"
+import Buma from "../skins/buma/layout.vue"
+const skin = "buma"
+var Layout;
+
+if (skin == "liberty") {
+    Layout = Liberty
+} else {
+    Layout = Buma
+}
+
+export default {
+    components: {
+        Layout
+    }
+}
+</script>
---
+++ skins/liberty/layout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>liberty</h1>
+        <nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
